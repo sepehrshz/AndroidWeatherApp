53d131a81b9056f971915a34ae7c1edf5ad7750a
sepehr1382 <sepehrshirazi27@gmail.com>
2025-04-20 21:46:33 +0330

    feat: add header + signup login button

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
@@ -27,7 +30,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherApp() {
-    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth().wrapContentSize(Alignment.Center)) {
+    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth()
+        .wrapContentSize(Alignment.TopStart)
+        .background(
+        brush = Brush.horizontalGradient(
+            colors = listOf(Color(0xFF138EFF), Color(0xFF6BB6FF))
+        )
+    )
+        ) {
         Header()
         TodayWeather()
         HourlyWeather()
